310f8956f9fea45b1166bbb8b492dd2b395168fb
Guan-Wei Huang <gwhuang24@gmail.com>
2024-10-31 23:54:13 -0400

    f=g+h

--- src/dataStructure.h
+++ src/dataStructure.h
@@ -7,21 +7,32 @@
 #define MAX_VOLUME 20.0     // Maximum volume of cargo in cubic meters
 
 // Define a structure for a Truck
-struct Truck
+typedef struct Truck
 {
 	struct Route route;  // Truck route infomation
 	double totalWeight;  // Total weight of cargo loaded
 	double totalVolume;  // Total volume of cargo loaded
-	double loadedPercentage; // Percentage of truck loaded
+	double loadedPersentage;
 	char routeSymbol;    // Symbol representing the truck and route color
-};
+}Truck;
 
 // Define a structure for a Shipment
-struct Shipment
+typedef struct Shipment
 {
+	struct Point point;
 	double weight;		// Weight of the individual shipment
 	double boxSize;     // Size of the box (e.g., volume or dimensions)
-	struct Point destination; // Destination point
-};
+}Shipment;
+
+// A algorithm
+typedef struct Node 
+{
+	struct Node* parent;   // Store parent's information
+	struct Point point;    // Store current position 
+	int g;    // distance between the current node and the start node.
+	int h;    // distance from the current node to the end node
+	int f;    // f = g + h;
+} Node;
+
 
 #endif
